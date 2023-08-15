4035db0365ad0339c7bce3433cc91cdfd8dac370
kondrat1206 <kondrat1206@gmail.com>
2023-08-15 11:27:52 +0300

    add new files

---
+++ Python_Core/07/7_1.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+result = setup(name='useful',
+      version='1',
+      description='Very useful code',
+      url='http://github.com/dummy_user/useful',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
+      packages=['useful'])
+
+
+print(result)
